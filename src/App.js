eb27b9228f43e0a870108f9f97d5ad52543c9011
ahmadaba14 <ahmadbinashraf@outlook.com>
2022-05-24 14:09:08 +0500

    Added Dashboard Route

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ const DefaultLayout = React.lazy(() => import('./layout/DefaultLayout'))
 const Login = React.lazy(() => import('./views/pages/login/Login'))
 const Page404 = React.lazy(() => import('./views/pages/page404/Page404'))
 const Page500 = React.lazy(() => import('./views/pages/page500/Page500'))
+const Dashboard = React.lazy(() => import('./views/dashboard/Dashboard'))
 
 class App extends Component {
   render() {
@@ -26,6 +27,7 @@ class App extends Component {
             <Route exact path="/404" name="Page 404" element={<Page404 />} />
             <Route exact path="/500" name="Page 500" element={<Page500 />} />
             <Route path="*" name="Home" element={<DefaultLayout />} />
+            <Route path="/dashboard" name="Dashboard" element={<Dashboard />} />
           </Routes>
         </Suspense>
       </BrowserRouter>
